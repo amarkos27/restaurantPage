1d68c8111507a4d07679b0901ca5b934e06a7f92
amarkos27 <armarkos27@gmail.com>
2024-04-29 12:47:30 -0400

    Style individual menu item

--- src/menu.js
+++ src/menu.js
@@ -7,7 +7,23 @@ export default function renderMenu() {
 
   const newItem = document.createElement('div');
   newItem.classList.add('menuItem');
+
+  const name = document.createElement('h2');
+  name.textContent = 'CHICKEN FILLET BURGER';
+  name.classList.add('itemName');
+
+  const description = document.createElement('p');
+  description.textContent = 'Whole bread fillet with classic or zinger coating';
+  description.classList.add('itemDescription');
+
+  const price = document.createElement('p');
+  price.textContent = '$3.99';
+  price.classList.add('itemPrice');
+
   newItem.appendChild(renderImage(ChickenFillet));
+  newItem.appendChild(name);
+  newItem.appendChild(description);
+  newItem.appendChild(price);
 
   container.appendChild(newItem);
 
